5d89189dd7fdbe38335e07e30189daff3045e718
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-02 01:39:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,8 +19,12 @@ def hopper
 
 end
 
+
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
+	programmer_hash ["alan_kay"]
+	
 	
 	programmer_hash = 
  		{
@@ -55,7 +59,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash = [dennis_ritchie: "languages"]
 end
+
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -64,7 +71,18 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
+
+
+	programmer_hash = {
+	    :yukihiro_matsumoto => {
+	     :known_for => "Ruby", 
+	      :languages => ["LISP", "C"]
+	    }
+}
+	end 
+	
+	
+ 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -80,8 +98,6 @@ def adding_matz
         }
      }
 
-    
-end
 
 def changing_alan
 	programmer_hash = 
@@ -102,8 +118,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][languages: "GUI"]
 end
+
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -116,10 +134,9 @@ def adding_to_dennis
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+        :dennis_ritchie => { }
      }
 
+      
+     programmer_hash[:dennis_ritchie] [language = "Assembly"]
 end
