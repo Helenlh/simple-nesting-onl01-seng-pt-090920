a1b99f85e707079c8f1d6c89a3f76d9a26f763c7
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-02 02:15:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,7 +41,7 @@ def alan_kay_is_known_for
      }
 end
 
-def dennis_ritchies_language
+def dennis_ritchies_languages
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -118,7 +118,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
    end 
-     programmer_hash[alan_kay][:languages] = "GUI"
+     programmer_hash[:alan_kay][:languages] = "GUI"
 
 
 
